51ec84ad6506bb22c29b1bf03d54ccc9ea7addb5
lagoonini <zaprojekat@proton.me>
2024-05-20 23:02:46 +0200

    Release 1 commit - Route drawing

--- frontend/src/app/invoice-view/invoice-view.component.ts
+++ frontend/src/app/invoice-view/invoice-view.component.ts
@@ -9,6 +9,16 @@ import { decode } from '@mapbox/polyline';
 import { jsPDF } from 'jspdf';
 import 'leaflet-simple-map-screenshoter';
 import 'leaflet-easyprint';
+import html2canvas from 'html2canvas';
+import autoTable from 'jspdf-autotable'
+
+
+declare module 'jspdf' {
+  interface jsPDF {
+    autoTable: (options: any) => jsPDF;
+    previous: any;
+  }
+}
 
 
 interface Coordinate {
@@ -42,15 +52,17 @@ interface Invoice {
   styleUrls: ['./invoice-view.component.scss']
 })
 export class InvoiceViewComponent implements OnInit, AfterViewInit {
-  @ViewChild('mapContainer', { static: false }) mapContainer!: ElementRef;
+  @ViewChild('mapContainer', {static: false}) mapContainer!: ElementRef;
   invoiceId: number = 0;
   invoice$: Observable<Invoice> = of();
   invoiceDetails: Invoice | undefined;
   private map: L.Map | undefined;
   mapInitialized = false;  // Flag to check if the map is initialized
+  private simpleMapScreenshoter: any;
 
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.invoiceId = +this.route.snapshot.paramMap.get('id')!;
@@ -71,13 +83,13 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
   loadInvoiceDetails(): void {
     if (this.invoiceId) { // Only make the HTTP request if invoiceId is not 0
       this.invoice$ = this.http.get<Invoice>(`http://localhost:8080/invoice/${this.invoiceId}`).pipe(
-        tap(invoice => {
-          this.invoiceDetails = invoice;
-          console.log('Invoice details:', this.invoiceDetails);
-          if (this.invoiceDetails) {
-            this.initMap();
-          }
-        })
+          tap(invoice => {
+            this.invoiceDetails = invoice;
+            console.log('Invoice details:', this.invoiceDetails);
+            if (this.invoiceDetails) {
+              this.initMap();
+            }
+          })
       );
       this.invoice$.subscribe();
     }
@@ -93,10 +105,12 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       const mapElement = document.getElementById('map');
       if (mapElement) {
-        this.map = L.map('map', {attributionControl: false,
+        this.map = L.map('map', {
+          attributionControl: false,
           zoomControl: false,
           fadeAnimation: false,
-          zoomAnimation: false}).setView([startCoordinate.latitude, startCoordinate.longitude], 13);
+          zoomAnimation: false
+        }).setView([startCoordinate.latitude, startCoordinate.longitude], 13);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '© OpenStreetMap contributors'
         }).addTo(this.map);
@@ -112,6 +126,10 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
           exportOnly: true,
           hideControlContainer: true,
           hidden: true // Hide the control, use programmatically
+        }).addTo(this.map);
+
+        this.simpleMapScreenshoter = L.simpleMapScreenshoter({
+          hidden: true, // Hide the screen icon
         }).addTo(this.map);
 
         // Store the printControl for later use
@@ -141,9 +159,9 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
       'Content-Type': 'application/json; charset=utf-8'
     });
 
-    const body = { coordinates: coordinates };
+    const body = {coordinates: coordinates};
 
-    this.http.post('https://api.openrouteservice.org/v2/directions/driving-car', body, { headers }).subscribe({
+    this.http.post('https://api.openrouteservice.org/v2/directions/driving-car', body, {headers}).subscribe({
       next: (response: any) => {
         if (!this.map) {
           console.error('Map is not initialized');
@@ -195,7 +213,7 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
         console.log('Valid LatLngs:', validLatLngs);
 
         // Draw the polyline on the map
-        const polyline = L.polyline(validLatLngs, { color: 'blue' }).addTo(this.map);
+        const polyline = L.polyline(validLatLngs, {color: 'blue'}).addTo(this.map);
         console.log('Polyline added:', polyline);
 
         // Adjust the map view to fit the route
@@ -240,9 +258,9 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
             });
 
             const address = await this.getAddress(coord.latitude, coord.longitude);
-            const marker = L.marker([latitudeOffset, longitudeOffset], { icon: customIcon })
-              .addTo(this.map)
-              .bindPopup(`Address: ${address}`);
+            const marker = L.marker([latitudeOffset, longitudeOffset], {icon: customIcon})
+                .addTo(this.map)
+                .bindPopup(`Address: ${address}`);
             console.log('Marker added:', marker);
           }
         });
@@ -263,15 +281,97 @@ export class InvoiceViewComponent implements OnInit, AfterViewInit {
   }
 
   public captureMap(): void {
-    if (this.map && (this.map as any).easyPrintControl) {
-      const printControl = (this.map as any).easyPrintControl;
+    if (this.map && this.simpleMapScreenshoter) {
       setTimeout(() => {
-        printControl.printMap('CurrentSize', 'myMap');
-      }, 1000); // Allow some time for the control to fully initialize
+        this.simpleMapScreenshoter.takeScreen('image').then((base64Image: string) => {
+          const mapContainer = document.getElementById('map'); // Use the parent container
+          if (mapContainer) {
+            const imgElement = document.createElement('img');
+            imgElement.src = base64Image;
+            imgElement.style.width = '100%';
+            imgElement.style.height = '100%';
+
+            // Clear the map container and append the captured image
+            mapContainer.innerHTML = '';
+            mapContainer.appendChild(imgElement);
+
+            // Optionally, save the image or use it further
+            console.log('Map captured and replaced with image.');
+          } else {
+            console.error('Map container not found.');
+          }
+        }).catch((error: any) => {
+          console.error('Error capturing map:', error);
+        });
+      }, 201); // Delay added to ensure the screenshot is taken after tiles are fully loaded
     } else {
-      console.error('Map is not initialized or print control is missing');
+      console.error('Map is not initialized or simpleMapScreenshoter is missing');
     }
   }
 
+  public exportPageAsPDF(): void {
+    const pdfContent = document.getElementById('pdf-content');
+    const mapElement = document.getElementById('map');
+
+    if (pdfContent && mapElement && this.invoiceDetails) {
+      html2canvas(mapElement).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+
+        // Title
+        pdf.setFontSize(18);
+        if (this.invoiceDetails) {
+          pdf.text(`Rechnung Nr: ${this.invoiceDetails.invoiceId}`, 10, 10);
+        }
+
+        // Add the table fields as text to the PDF
+        pdf.setFontSize(12);
+        if (this.invoiceDetails) {
+          const createdDate = new Date(this.invoiceDetails.createdDate); // Ensure createdDate is a Date object
+          const formattedDate = createdDate.toLocaleString('en-US', {
+            month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true
+          });
+
+          // Using autoTable to add a table to the PDF
+          autoTable(pdf, {
+            startY: 20,
+            head: [['Rechnungsdetails', '']],
+            body: [
+              [{ content: 'Invoice ID', styles: { fontStyle: 'bold' } }, `${this.invoiceDetails.invoiceId}`],
+              [{ content: 'Created Date', styles: { fontStyle: 'bold' } }, `${formattedDate}`],
+              [{ content: 'Distance', styles: { fontStyle: 'bold' } }, `${this.invoiceDetails.distance.toFixed(2)} km`],
+              [{ content: 'Total Cost', styles: { fontStyle: 'bold' } }, `${this.invoiceDetails.totalCost.toFixed(2)} EUR`],
+              [{ content: 'Route ID', styles: { fontStyle: 'bold' } }, `${this.invoiceDetails.route.id}`],
+              [{ content: 'Vehicle ID', styles: { fontStyle: 'bold' } }, `${this.invoiceDetails.route.vehicle.id}`],
+            ],
+            theme: 'striped',
+            styles: {
+              fontSize: 12
+            },
+            headStyles: {
+              fillColor: [220, 220, 220]
+            },
+            bodyStyles: {
+              cellPadding: 2,
+            },
+          });
+        }
+
+        // Add the map image to the PDF
+        const imgWidth = 180; // Adjust width to fit the content
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        pdf.addImage(imgData, 'PNG', 10, (pdf as any).lastAutoTable.finalY + 10, imgWidth, imgHeight);
+
+        // Save the PDF
+        pdf.save('invoice_details.pdf');
+
+        console.log('Page exported as PDF.');
+      }).catch((error: any) => {
+        console.error('Error capturing map:', error);
+      });
+    } else {
+      console.error('PDF content or map element not found, or invoice details missing.');
+    }
+  }
 
 }
